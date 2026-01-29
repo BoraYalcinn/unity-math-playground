bbd7b8982ff4027e7f9e7841514db0a10c28d797
BoraYalcinn <borayalcintr@gmail.com>
2026-01-29 14:17:22 +0300

    created the laser and Raycast. So we can draw a sphere on hit

--- Assets/Scripts/LaserBounce.cs
+++ Assets/Scripts/LaserBounce.cs
@@ -1,8 +1,27 @@
 using UnityEngine;
 using System;
+using UnityEngine.PlayerLoop;
 
 
 public class LaserBounce : MonoBehaviour
 {
-    
+
+
+    public void OnDrawGizmos()
+    {
+        
+        Vector2 origin = transform.position;
+        Vector2 direction = transform.right; // x axis
+        
+        Ray ray = new Ray(origin, direction);
+        
+        Gizmos.DrawLine(origin, origin + direction);
+
+        if (Physics.Raycast(ray,out RaycastHit hit))
+        {
+            Gizmos.DrawSphere(hit.point,0.1f);
+        }
+        
+        
+    }
 }
