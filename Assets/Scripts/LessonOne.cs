b07bda4cd1e4e17c327089e790439edcda43c287
BoraYalcinn <borayalcintr@gmail.com>
2026-01-29 12:21:35 +0300

    Created visible vectors and implemented Normalization

--- Assets/Scripts/LessonOne.cs
+++ Assets/Scripts/LessonOne.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using System;
 
 public class LessonOne : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform A;
+    public Transform B;
 
-    // Update is called once per frame
-    void Update()
+    public float scProj;
+
+    void OnDrawGizmos()
     {
+        if (A == null || B == null)
+        {
+            return;
+        }
+
+        Vector2 a = A.position;
+        Vector2 b = B.position;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(default,a);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(default,b);
         
+        // Normalization
+        float aLen = Mathf.Sqrt(a.x * a.x + a.y * a.y);
+        float bLen = Mathf.Sqrt(b.x * b.x + b.y * b.y);
+        Vector2 NormalizedA = a / aLen;
+        Vector2 NormalizedB = b/bLen;
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(NormalizedA, 0.05f);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(NormalizedB, 0.05f);
+        
+        // Scalar Projection
+
     }
+    
+    
 }
