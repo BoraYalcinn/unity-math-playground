4ea6519a6c1078caf27cb3105b0b155f7d393f20
BoraYalcinn <borayalcintr@gmail.com>
2026-01-29 13:22:39 +0300

    radial trigger implemented

--- Assets/Scripts/RadialTrigger.cs
+++ Assets/Scripts/RadialTrigger.cs
@@ -17,13 +17,19 @@ public class RadialTrigger : MonoBehaviour
         // Radial Trigger
         Vector3 center = transform.position;
         Gizmos.DrawWireSphere(center,radius);
+
+        if (C == null) return;
         
         Vector2  c = C.position;
         
         Gizmos.color = Color.green;
         Gizmos.DrawLine(center,c);
 
+        float dist =  Vector2.Distance(center, c);
+        bool inside =  dist <= radius;
         
+        Gizmos.color = inside ? Color.red : Color.green;
+        Gizmos.DrawSphere(center,radius);
         
     }
     
