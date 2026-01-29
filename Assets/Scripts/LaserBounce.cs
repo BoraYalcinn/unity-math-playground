5a13a0ab8814e060139f13a3dff279cf1a0b864c
BoraYalcinn <borayalcintr@gmail.com>
2026-01-29 15:00:49 +0300

    implemented the reflection vector

--- Assets/Scripts/LaserBounce.cs
+++ Assets/Scripts/LaserBounce.cs
@@ -16,12 +16,25 @@ public class LaserBounce : MonoBehaviour
         Ray ray = new Ray(origin, direction);
         
         Gizmos.DrawLine(origin, origin + direction);
-
+        
+        
+        
         if (Physics.Raycast(ray,out RaycastHit hit))
         {
             Gizmos.DrawSphere(hit.point,0.1f);
+            // Vector2 reflection = direction + (hit.normal * (-2) * Vector2.Dot(direction, hit.normal));  ***formula for the reflection***
+            // writing a seperate function for calculating the reflection vector will be much more useful 
+            Vector2 reflection = ReflectVector(hit.normal,ray.direction);
+            // there is also a built-in reflect function also but I will not be using it
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(hit.point, hit.point + reflection);
         }
         
-        
     }
+
+    Vector2 ReflectVector(Vector2 normal, Vector2 direction)
+    {
+        return direction + (normal * (-2) * Vector2.Dot(direction, normal));
+    }
+    
 }
