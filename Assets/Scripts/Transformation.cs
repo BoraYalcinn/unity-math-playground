4b8b95a620d03f94ff272bc3e06b9223cb414afb
BoraYalcinn <borayalcintr@gmail.com>
2026-01-29 18:10:38 +0300

    LocalToWorld fixed and WorldToLocal implemented

--- Assets/Scripts/Transformation.cs
+++ Assets/Scripts/Transformation.cs
@@ -4,17 +4,27 @@ public class Transformation : MonoBehaviour
 {
 
     public Vector2 localCoord;
+    public Vector2 worldCoord;
+    
     
     public void OnDrawGizmos()
     {
         Vector2 worldPos = LocalToWorld(localCoord);
+        Gizmos.DrawSphere(worldPos, 0.1f);
         
-        Gizmos.DrawSphere(localCoord, 0.1f);
-        Gizmos.DrawLine(default,worldPos);
-
+        Vector2 localPos = WorldToLocal(worldCoord);
+        Gizmos.DrawSphere(localPos,0.1f);
 
     }
 
+    Vector2 WorldToLocal(Vector2 worldPos)
+    {
+        Vector2 rel = worldPos - (Vector2)transform.position;
+        float x = Vector2.Dot(rel, transform.right); // x axis
+        float y = Vector2.Dot(rel, transform.up); // y axis
+        return new(x,y);
+    }
+    
     Vector2 LocalToWorld(Vector2 localCoord)
     {
         Vector2 position = transform.position;
