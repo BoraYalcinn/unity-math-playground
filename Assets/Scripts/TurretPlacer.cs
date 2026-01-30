04967a7ac524016b36c3248c20afca63db906b43
BoraYalcinn <borayalcintr@gmail.com>
2026-01-31 00:31:11 +0300

    adding kinda like a third person camera now its time for the cross product

--- Assets/Scripts/TurretPlacer.cs
+++ Assets/Scripts/TurretPlacer.cs
@@ -21,6 +21,11 @@ public class TurretPlacer : MonoBehaviour
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             turret.transform.position = hit.point;
+            turret.rotation = Quaternion.LookRotation(ray.direction);
+            //  after only adding the above 2 lines our turret will face away from the camera
+            // but in some angles it will just look directly down into the ground which is something we
+            // don't want . In this case we cann apply cross product with the surface which will give us another
+            // vector thats perpendicular.
         }
 
     }
