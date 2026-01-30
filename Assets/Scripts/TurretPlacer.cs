ed23c0108f65f04166a0cf1ce2082fb74e768c09
BoraYalcinn <borayalcintr@gmail.com>
2026-01-31 01:11:43 +0300

    applied cross product and drew directions with comment blocks explaining

--- Assets/Scripts/TurretPlacer.cs
+++ Assets/Scripts/TurretPlacer.cs
@@ -21,11 +21,33 @@ public class TurretPlacer : MonoBehaviour
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             turret.transform.position = hit.point;
-            turret.rotation = Quaternion.LookRotation(ray.direction);
+            /*
+              turret.rotation = Quaternion.LookRotation(ray.direction);
+            */
+            
             //  after only adding the above 2 lines our turret will face away from the camera
             // but in some angles it will just look directly down into the ground which is something we
             // don't want . In this case we cann apply cross product with the surface which will give us another
             // vector thats perpendicular.
+
+            Vector3 yAxis = hit.normal;
+            Vector3 xAxis = Vector3.Cross(yAxis,ray.direction);
+            xAxis = xAxis.normalized; // normalize this so that it always has a spesific length
+            
+            Vector3 zAxis = Vector3.Cross(xAxis, yAxis); // its direction our turret is looking at
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(hit.point, xAxis); // After cross-product
+            
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(hit.point, yAxis); // it's the normal to the surface 
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(hit.point, zAxis);
+
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(ray.origin,hit.point);
+            
         }
 
     }
