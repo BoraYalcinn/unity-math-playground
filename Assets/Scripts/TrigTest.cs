f55e7867d2232026ed267bd3ba16ea2170dc6537
BoraYalcinn <borayalcintr@gmail.com>
2026-02-02 21:09:00 +0300

    add TrigTest script and animate 1 turn per seconds

---
+++ Assets/Scripts/TrigTest.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System;
+using UnityEditor;
+using UnityEngine.Rendering;
+
+public class TrigTest : MonoBehaviour
+{
+
+    [Range(0, 360)] 
+    public float angDeg =  0;
+    
+    void OnDrawGizmos()
+    {
+
+        Handles.DrawWireDisc(Vector3.zero,Vector3.forward,1);
+        
+        // float angRad =  angDeg * Mathf.Deg2Rad;
+
+        float angTurns = (float)EditorApplication.timeSinceStartup;
+        Vector2 v = AngToDir(angTurns*Mathf.PI *2 ); //  1 turn per second ! 
+        Gizmos.DrawRay(default,v);
+        
+    }
+
+    static Vector2 AngToDir(float angRad)
+    {
+        return new Vector2(Mathf.Cos(angRad),Mathf.Sin(angRad));
+    }
+    
+}
