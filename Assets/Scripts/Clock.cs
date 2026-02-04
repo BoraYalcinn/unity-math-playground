a06e786d3e087daa7c21239772e9e25691536e24
BoraYalcinn <borayalcintr@gmail.com>
2026-02-04 22:10:08 +0300

    created ticks for the clock

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -6,23 +6,48 @@ public class Clock : MonoBehaviour
     
     [Range(0,60)]
     public float secondsTest;
-
-    public float radius = 1;
+    
+    [Range(0,0.2f)]
+    public float tickSizeSecMin = 0.05f;
+    
+    public const float radius = 1;
     
     void OnDrawGizmos()
     {
         Handles.matrix = Gizmos.matrix = transform.localToWorldMatrix;
         Handles.DrawWireDisc(default,Vector3.forward, radius );
-
-        Gizmos.DrawRay(default,radius * SecondsToDirection(secondsTest));
+        
+        // ticks (minutees/seconds)
+        for (int i = 0; i < 60; i++)
+        {
+            Vector2 dir = SecondsOrMinutesToDirection(i);
+            DrawTick(dir,tickSizeSecMin,1);
+        }
+        
+        
+        Gizmos.DrawRay(default,radius * SecondsOrMinutesToDirection(secondsTest));
     }
     
     Vector2 AngToDir(float angleRad) => new Vector2(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
-    
-    Vector3 SecondsToDirection(float seconds)
+
+    void DrawTick(Vector2 dir, float length, float thickness)
     {
-        float t = seconds / 60; // percent along the 0-60 range (1-0 representation)
-        float angleRad =  t * Mathf.PI * 2;
+        Handles.DrawLine(dir,dir * (1f - length),thickness);
+        
+    }
+    
+    Vector3 SecondsOrMinutesToDirection(float secOrMin)
+    {
+        float t = secOrMin / 60; // percent along the 0-60 range (1-0 representation)
+        return FractionToClockDir(t);
+    }
+    
+    
+    
+    
+    Vector2 FractionToClockDir(float t)
+    {
+        float angleRad = (0.25f - t) * Mathf.PI * 2;
         return AngToDir(angleRad);
     }
     
